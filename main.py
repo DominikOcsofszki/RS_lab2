ae4f8dcb0665412cb7b06f53bb2623798be0ba5c
dominikocsofszki <10422001@student.vgu.edu.vn>
2023-05-30 10:08:39 +0700

    adding x extra camera moduls

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import time
 import scheduler
 from task1 import *
 from task2 import *
+from task_index_cam import *
 
 scheduler = scheduler.Scheduler()
 scheduler.SCH_Init()
-task1 = Task1()
+# task1 = Task1()
+task1 = Task_Index_Cam(0)
+task2 = Task_Index_Cam(0)
 # task2 = Task2()
 
 
-scheduler.SCH_Add_Task(task1.Task1Run, 1000, 5000)
-# scheduler.SCH_Add_Task(task2.Task2Run, 2000, 4000)
+scheduler.SCH_Add_Task(task1.Task_Index_Cam_run, 0, 5000)
+scheduler.SCH_Add_Task(task2.Task_Index_Cam_run, 2000, 5000)
 
 while True:
     scheduler.SCH_Update()
